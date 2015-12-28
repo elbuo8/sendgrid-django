8707e4fdeabbb0766bf04a82691193ce9d3d0814
John Finkelstein <johnf@healthvana.com>
2015-12-28 13:29:12 -0800

    Support asm_group_id option.

--- sgbackend/mail.py
+++ sgbackend/mail.py
@@ -105,6 +105,9 @@ class SendGridBackend(BaseEmailBackend):
             if "Filters" in email.extra_headers:
                 mail.smtpapi.data['filters'] = email.extra_headers["Filters"]
 
+            if "Group" in email.extra_headers:
+                mail.data['asm_group_id'] = email.extra_headers["Group"]
+
             for attachment in email.attachments:
                 mail.add_attachment_stream(attachment[0], attachment[1])
 
