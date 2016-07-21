26102630eb644c2518ea59d7e3696e6c3579cb2d
Roel Obdam <roel@purewebsolutions.nl>
2016-07-21 15:19:34 +0200

    Sendgrid API v3 compatibility
    Basics seem to work

--- sgbackend/mail.py
+++ sgbackend/mail.py
@@ -4,6 +4,8 @@ from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from email.mime.base import MIMEBase
 import sendgrid
+import base64
+from sendgrid.helpers.mail import *
 
 try:
     import rfc822
@@ -19,22 +21,13 @@ class SendGridBackend(BaseEmailBackend):
         super(SendGridBackend, self).__init__(
             fail_silently=fail_silently, **kwargs)
         self.api_key = getattr(settings, "SENDGRID_API_KEY", None)
-        self.api_user = getattr(settings, "SENDGRID_USER", None)
-        self.api_password = getattr(settings, "SENDGRID_PASSWORD", None)
 
-        credentials = []
         if self.api_key:
-            credentials.append(self.api_key)
-        elif self.api_user and self.api_password:
-            credentials.append(self.api_user)
-            credentials.append(self.api_password)
+            self.sg = sendgrid.SendGridAPIClient(
+                apikey=self.api_key)
         else:
             raise ImproperlyConfigured('''
-                Either SENDGRID_API_KEY or both (SENDGRID_USER and
-                SENDGRID_PASSWORD) must be declared in settings.py''')
-        self.sg = sendgrid.SendGridClient(
-            *credentials,
-            raise_errors=not fail_silently)
+                SENDGRID_API_KEY must be declared in settings.py''')
 
     def open(self):
         pass
@@ -51,61 +44,70 @@ class SendGridBackend(BaseEmailBackend):
 
         count = 0
         for email in emails:
-            try:
-                self.sg.send(self._build_sg_mail(email))
-                count += 1
-            except sendgrid.SendGridClientError:
-                if not self.fail_silently:
-                    raise
-            except sendgrid.SendGridServerError:
-                if not self.fail_silently:
-                    raise
+            mail = self._build_sg_mail(email)
+            response = self.sg.client.mail.send.post(request_body=mail)
+            count += 1
 
         return count
 
     def _build_sg_mail(self, email):
-        mail = sendgrid.Mail()
-        mail.add_to(email.to)
-        mail.add_cc(email.cc)
-        mail.add_bcc(email.bcc)
-        mail.set_text(email.body)
+        mail = Mail()
+        mail.set_from(Email(email.from_email))
         mail.set_subject(email.subject)
-        mail.set_from(email.from_email)
 
+        personalization = Personalization()
+        for e in email.to:
+            personalization.add_to(Email(e))
+        for e in email.cc:
+            personalization.add_cc(Email(e))
+        for e in email.bcc:
+            personalization.add_bcc(Email(e))
+        personalization.set_subject(email.subject)
+        mail.add_content(Content("text/plain", email.body))
         if isinstance(email, EmailMultiAlternatives):
             for alt in email.alternatives:
                 if alt[1] == "text/html":
-                    mail.set_html(alt[0])
+                    mail.add_content(Content(alt[1], alt[0]))
+        elif email.content_subtype == "html":
+            mail.contents = []
+            mail.add_content(Content("text/plain", ' '))
+            mail.add_content(Content("text/html", email.body))
 
         for attachment in email.attachments:
             if isinstance(attachment, MIMEBase):
-                mail.add_attachment_stream(
-                    attachment.get_filename(),
-                    attachment.get_payload())
+                attach = Attachment()
+                attach.set_filename(attachment.get_filename())
+                attach.set_content(base64.b64encode(attachment.get_payload()))
+                mail.add_attachment(attach)
             elif isinstance(attachment, tuple):
-                mail.add_attachment_stream(attachment[0], attachment[1])
+                attach = Attachment()
+                attach.set_filename(attachment[0])
+                attach.set_content(base64.b64encode(attachment[1]))
+                attach.set_type(attachment[2])
+                mail.add_attachment(attach)
 
-        if email.extra_headers:
-            if "Reply-To" in email.extra_headers:
-                reply_to = rfc822.parseaddr(email.extra_headers["Reply-To"])[1]
-                mail.set_replyto(reply_to)
+                # if email.extra_headers:
+                # if "Reply-To" in email.extra_headers:
+                # reply_to = rfc822.parseaddr(email.extra_headers["Reply-To"])[1]
+                # personalization.set_replyto(reply_to)
 
-            if "Subs" in email.extra_headers:
-                mail.set_substitutions(email.extra_headers["Subs"])
+                # if "Subs" in email.extra_headers:
+                #    mail.set_substitutions(email.extra_headers["Subs"])
 
-            if "Sections" in email.extra_headers:
-                mail.set_sections(email.extra_headers["Sections"])
+                # if "Sections" in email.extra_headers:
+                #    mail.set_sections(email.extra_headers["Sections"])
 
-            if "Categories" in email.extra_headers:
-                mail.set_categories(email.extra_headers["Categories"])
+                # if "Categories" in email.extra_headers:
+                #    mail.set_categories(email.extra_headers["Categories"])
 
-            if "Unique-Args" in email.extra_headers:
-                mail.set_unique_args(email.extra_headers["Unique-Args"])
+                # if "Unique-Args" in email.extra_headers:
+                #    mail.set_unique_args(email.extra_headers["Unique-Args"])
 
-            if "Filters" in email.extra_headers:
-                mail.smtpapi.data['filters'] = email.extra_headers["Filters"]
+                # if "Filters" in email.extra_headers:
+                #    mail.smtpapi.data['filters'] = email.extra_headers["Filters"]
 
-            for attachment in email.attachments:
-                mail.add_attachment_stream(attachment[0], attachment[1])
+                # for attachment in email.attachments:
+                #    mail.add_attachment_stream(attachment[0], attachment[1])
 
-        return mail
+        mail.add_personalization(personalization)
+        return mail.get()
