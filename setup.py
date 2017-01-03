292d0251164d9c084a135fce0cdcf91ba00e5ce9
Alexander Kavanaugh <alex@kavdev.io>
2017-01-03 11:55:16 -0800

    Added trove classifiers (#41)
    Definitely double check against [this list](https://pypi.python.org/pypi?%3Aaction=list_classifiers) and let me know if you want anything added/removed.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+,from setuptools import setup, find_packages
 
 __version__ = None
 with open('sgbackend/version.py') as f:
@@ -15,4 +15,18 @@ setup(
     description='SendGrid Backend for Django',
     long_description=open('./README.rst').read(),
     install_requires=["sendgrid >= 3.5, < 4"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django".
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Communications :: Email",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
