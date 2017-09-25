4b9c6da218bc389ebc347765a285d15ee36d9969
Jay Hale <jay@jtst.io>
2017-09-25 17:19:01 -0400

    Upgrading sendgrid dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='MIT',
     description='SendGrid Backend for Django',
     long_description=open('./README.rst').read(),
-    install_requires=["sendgrid >= 3.5, < 4"],
+    install_requires=["sendgrid == 5.2.0"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
